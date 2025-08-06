992a082c6689912bfa046bb15771d3da47293659
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-06 19:20:44 +0000

    0. Email Junit

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,17 +1,26 @@
-public class TesteEmail {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    /*
-     * 
-     * testar_email_sem_arroba deve verificar se o método emailValid() ao receber como parametro “email_testedominio.com.br” deve testar - utilize assertFalse
-     * testar_email_mais_50_caracteres deve verificar se o método emailValid() ao receber como parametro “email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br” retorna false testar com assertEquals.
-     */
+public class TesteEmail {
 
      //testar_email_com_arroba deve verificar se o método emailValid() ao receber como parametro “email_teste@dominio.com.br” retorna true - utilize assertTrue
     @Test
     void testar_email_com_arroba() {
-        Calculator calculator = new Calculator();
-        int expected = 5;
-        int actual = calculator.add(2, 3);
-        assertEquals(expected, actual, "The sum of 2 and 3 should be 5");
+         assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
     }
+
+    //testar_email_sem_arroba deve verificar se o método emailValid() ao receber como parametro “email_testedominio.com.br” deve testar - utilize assertFalse
+    @Test
+    void testar_email_sem_arroba() {
+        assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
+    }
+
+
+    //testar_email_mais_50_caracteres deve verificar se o método emailValid() ao receber como parametro “email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br” retorna false testar com assertEquals.
+    @Test
+    void testar_email_mais_50_caracteres() {
+        assertEquals(false,Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+    }
+
 }
